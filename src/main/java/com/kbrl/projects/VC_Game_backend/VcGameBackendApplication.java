29351beadf396e7d7fdec592a4b171a12859142b
mayanknvu <mayanknvu@gmail.com>
2025-05-16 19:46:39 +0530

    Spring Commit

---
+++ src/main/java/com/kbrl/projects/VC_Game_backend/VcGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kbrl.projects.VC_Game_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbrl/projects/VC_Game_backend/VcGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbrl.projects.VC_Game_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
